ea6f4960345ad85586b357071b0abce5119e2b5c
Sean Grove <s@bushi.do>
2014-03-01 18:29:53 -0800

    Start converting components to emit :key properly, so React can speed things up quite a bit

--- src/omchaya/utils.cljs
+++ src/omchaya/utils.cljs
@@ -52,10 +52,10 @@
         hash (crypt/byteArrayToHex (.digest container))]
     (str "http://gravatar.com/avatar/" hash "?s=30&d=identicon")))
 
-(defn gravatar-for [email]
+(defn gravatar-for [email & [optional-uid]]
   [:img.avatar
-   {:src
-    (email->gravatar-url email)}])
+   {:key (str email optional-uid)
+    :src (email->gravatar-url email)}])
 
 (defn set-window-href! [path]
   (js/window.history.pushState #js {}, "", path))
